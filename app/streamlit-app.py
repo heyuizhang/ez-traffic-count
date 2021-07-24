44c81526be77e915540e20312844804365a2d725
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2021-07-24 22:50:42 +0000

    temp2

--- app/streamlit-app.py
+++ app/streamlit-app.py
@@ -37,3 +37,39 @@ def load_obj_detector(config, wt_file):
     wrapper func to load and cache object detector 
     """
     obj_detector = tc.ObjectDetector(wt_file, config, confidence = def_values['conf']/100,
+     nms_threshold=def_values['nms']/100)
+
+    return obj_detector
+    
+    
+
+def parameter_sliders(key, enabled = True, value = None):
+    conf = custom_slider("Model Confidence", 
+                        minVal = 0, maxVal = 100, InitialValue= value[0], enabled = enabled,
+                        key = key[0])
+    nms = custom_slider('Overlapping Threshold', 
+                        minVal = 0, maxVal = 100, InitialValue= value[1], enabled = enabled,
+                        key = key[1])
+
+        
+    return(conf, nms)
+
+
+def trigger_rerun():
+    """
+    mechanism in place to force resets and update widget states
+    """
+    session_infos = Server.get_current()._session_info_by_id.values() 
+    for session_info in session_infos:
+        this_session = session_info.session
+    this_session.request_rerun()
+
+def main():
+    st.set_page_config(page_title = "Traffic Flow Counter", 
+    page_icon=":vertical_traffic_light:")
+
+    obj_detector = load_obj_detector(config, wt_file)
+    tracker = tc.CarsInFrameTracker(num_previous_frames = 10, frame_shape = (720, 1080))
+
+    state = SessionState.get(upload_key = None, enabled = True, 
+    start = False, conf = 70, nms = 50, run = False)
