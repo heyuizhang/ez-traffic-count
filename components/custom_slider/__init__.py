745cdb2d8478495b0e5877d01995596acc9503c2
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2021-10-01 17:23:01 +0000

    removed white space

--- components/custom_slider/__init__.py
+++ components/custom_slider/__init__.py
@@ -1,4 +1,5 @@
 import os
 import streamlit.components.v1 as components
 
-root_dir = os.path.dirname(os.path.abspath(__fil
+root_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join
