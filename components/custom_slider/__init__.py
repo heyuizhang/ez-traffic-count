d17f6252b48a45ff9dbc03237cd22b4dc9c99078
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2021-10-05 09:57:21 +0000

    tuned message;

--- components/custom_slider/__init__.py
+++ components/custom_slider/__init__.py
@@ -2,4 +2,6 @@ import os
 import streamlit.components.v1 as components
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
-build_dir = os.path.join
+build_dir = os.path.join(root_dir, 'frontend/build')
+
+# To
