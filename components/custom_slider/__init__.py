0969e0bd0b99b780c040a56cbeec64af4511504c
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2021-10-10 11:19:39 +0000

    unused function

--- components/custom_slider/__init__.py
+++ components/custom_slider/__init__.py
@@ -4,4 +4,5 @@ import streamlit.components.v1 as components
 root_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(root_dir, 'frontend/build')
 
-# To
+# Toggle between dev and production mode
+_RE
