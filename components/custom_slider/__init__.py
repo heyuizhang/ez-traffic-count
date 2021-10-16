03976bc66062fa6148961bd7999aeaa5363d5f2f
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2021-10-16 05:49:32 +0000

    Update h04.md

--- components/custom_slider/__init__.py
+++ components/custom_slider/__init__.py
@@ -5,4 +5,7 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(root_dir, 'frontend/build')
 
 # Toggle between dev and production mode
-_RE
+_RELEASE = True
+
+if _RELEASE:
+    _component_func 
