7c1065171519f8d9dec63d7f84c94bbb98d044f0
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2021-10-31 16:03:40 +0000

    more test

--- components/custom_slider/__init__.py
+++ components/custom_slider/__init__.py
@@ -29,4 +29,4 @@ def custom_slider(label: str, minVal: int, maxVal: int, enabled: bool,
 
 # Add some test code to play with the component while it's in development.
 # During development, we can run this just as we would any other Streamlit
-# app: `
+# app: `$ streamlit run my_component/__i
