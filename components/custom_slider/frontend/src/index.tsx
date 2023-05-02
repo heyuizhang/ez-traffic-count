e35cf5a3cf32f652c5639360a922a35a2e4c3427
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2023-05-02 08:57:55 +0000

    ya

--- components/custom_slider/frontend/src/index.tsx
+++ components/custom_slider/frontend/src/index.tsx
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom"
 import CustomSlider from "./custom_slider"
 
 import { Client as Styletron } from "styletron-engine-atomic"
-import { 
+import { Provider as StyletronProvider 
