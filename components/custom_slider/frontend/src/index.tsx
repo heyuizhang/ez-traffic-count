edec057a1958a3d5ceefcec25f696d17b09acd1d
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2023-05-02 05:47:07 +0000

    firmware: Upgrade to latest

--- components/custom_slider/frontend/src/index.tsx
+++ components/custom_slider/frontend/src/index.tsx
@@ -2,4 +2,5 @@ import React from "react"
 import ReactDOM from "react-dom"
 import CustomSlider from "./custom_slider"
 
-import { Client as Styletron } from "st
+import { Client as Styletron } from "styletron-engine-atomic"
+import { 
