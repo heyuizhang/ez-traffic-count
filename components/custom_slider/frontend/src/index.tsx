6ddea3b48df1becd6a9e0e5e2d9896f3e3b5a934
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2023-05-07 12:27:41 +0000

    Fix download url

--- components/custom_slider/frontend/src/index.tsx
+++ components/custom_slider/frontend/src/index.tsx
@@ -4,4 +4,4 @@ import CustomSlider from "./custom_slider"
 
 import { Client as Styletron } from "styletron-engine-atomic"
 import { Provider as StyletronProvider } from "styletron-react"
-import {
+import { ThemeProvider, LightTheme } from "b
