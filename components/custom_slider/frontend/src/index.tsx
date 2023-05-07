578262826916551c08b23c05cc810388d33b7b7f
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2023-05-07 14:24:08 +0000

    Update license identifier

--- components/custom_slider/frontend/src/index.tsx
+++ components/custom_slider/frontend/src/index.tsx
@@ -4,4 +4,8 @@ import CustomSlider from "./custom_slider"
 
 import { Client as Styletron } from "styletron-engine-atomic"
 import { Provider as StyletronProvider } from "styletron-react"
-import { ThemeProvider, LightTheme } from "b
+import { ThemeProvider, LightTheme } from "baseui"
+
+const engine = new Styletron()
+
+/
