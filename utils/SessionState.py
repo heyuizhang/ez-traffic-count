2df0b91b8eebb1d06499b9640e94c230a3d63f06
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2023-07-08 13:38:11 +0000

    Atualizacao

--- utils/SessionState.py
+++ utils/SessionState.py
@@ -9,4 +9,4 @@ Usage
 >>> session_state.user_name = 'Mary'
 >>> session_state.favorite_color
 'black'
-Since you set user_name above,
+Since you set user_name above, next time your script runs this will
