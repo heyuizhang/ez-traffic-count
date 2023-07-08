96360e0855c918c29612afe3b7df6083df25e077
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2023-07-08 07:30:39 +0000

    Update french translation.

--- utils/SessionState.py
+++ utils/SessionState.py
@@ -6,4 +6,5 @@ Usage
 >>> session_state = SessionState.get(user_name='', favorite_color='black')
 >>> session_state.user_name
 ''
->>> session_state.user_name = 
+>>> session_state.user_name = 'Mary'
+>>> session_state.favorite_color
