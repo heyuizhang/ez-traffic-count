54aa4e756ae7cd098cbaba85574eab80cc8a4f4e
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2023-08-04 18:24:03 +0000

    Update application.conf

--- utils/SessionState.py
+++ utils/SessionState.py
@@ -20,4 +20,5 @@ try:
     from streamlit.server.Server import Server
 except Exception:
     # Streamlit >= 0.65.0
-    import streamlit.report_thread as ReportTh
+    import streamlit.report_thread as ReportThread
+    from streamlit.server.server import Serve
