51d0dd5e547f565177f1f6f07d95891c89f9fe1c
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2023-09-12 14:17:59 +0000

    Update h11.md

--- utils/SessionState.py
+++ utils/SessionState.py
@@ -49,4 +49,4 @@ def get(**kwargs):
     Parameters
     ----------
     **kwargs : any
-        Default values you want to add to the
+        Default values you want to add to the session state, if we're c
