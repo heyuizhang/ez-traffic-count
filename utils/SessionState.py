22ee926203059c7d6255db507ce7c5416274b8b1
heyuizhang <135280680+heyuizhang@users.noreply.github.com>
2023-11-08 06:09:20 +0000

    fix cs

--- utils/SessionState.py
+++ utils/SessionState.py
@@ -64,4 +64,5 @@ def get(**kwargs):
     >>> session_state = get(user_name='', favorite_color='black')
     >>> session_state.user_name
     'Mary'
-    "
+    """
+    # Hack to get the session obje
